9d34a8680e511c252e88e2a82b0aba0e0d43925d
strawbreey <2675882608@qq.com>
2021-10-14 20:50:13 +0800

    feat: add route guard

--- src/app/interceptors/https.interceptor.ts
+++ src/app/interceptors/https.interceptor.ts
@@ -3,9 +3,12 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpResponse
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap, debounceTime } from 'rxjs/operators';
+
 
 @Injectable()
 export class HttpsInterceptor implements HttpInterceptor {
@@ -13,7 +16,22 @@ export class HttpsInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    console.log(request)
-    return next.handle(request);
+    const req = request.clone({setHeaders: {
+      'X-Requested-With': 'XMLHttpRequest',
+      'request-id': Math.random().toString(36).slice(-8) +  '-' + new Date().getTime()
+    }});
+    console.log('request: loading');
+    this.
+    
+    return next.handle(req).pipe(debounceTime(1000)).pipe(tap(event => {
+      console.log(event)
+      console.log('request: event')
+    }, error => {
+      console.log(error)
+      console.log('request: error')
+      if (error.status === 401 && location.pathname !== '/login') {
+        location.href = 'login?return_url=' + encodeURIComponent(location.href);
+      }
+    }));
   }
 }
