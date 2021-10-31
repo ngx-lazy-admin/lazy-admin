d452dfc347ec7ba07ccf5344cf62c017e511e96c
strawbreey <2675882608@qq.com>
2021-10-31 23:22:27 +0800

    feat: update modal

--- src/app/modules/modal/modal-form.component.ts
+++ src/app/modules/modal/modal-form.component.ts
@@ -1,12 +1,7 @@
 
-/* declarations: NzModalCustomComponent */
-import { Component, OnInit, Input, TemplateRef, ViewContainerRef, Renderer2, Optional   } from '@angular/core';
-import { NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
-import hotkeys from 'hotkeys-js';
-
-import { FormGroup, AbstractControl } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { FormlyFieldConfig } from '@ngx-formly/core';
-import { ModalService } from './modal.service';
 
 @Component({
   selector: 'nz-modal-custom-component',
@@ -18,10 +13,4 @@ export class NzModalCustomComponent {
   @Input() model?: any;
 
   form = new FormGroup({});
-
-  constructor(private modal: NzModalRef) {}
-
-  destroyModal(): void {
-    this.modal.destroy({ data: 'this the result data' });
-  }
 }
