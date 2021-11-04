33f925e0ce40f75e98e50b2c04588f191b29cfcc
strawbreey <2675882608@qq.com>
2021-11-05 01:07:11 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -58,9 +58,9 @@ registerLocaleData(zh);
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
-    // HttpClientInMemoryWebApiModule.forRoot(
-    //   InMemoryDataService, { dataEncapsulation: false }
-    // )
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [
     httpInterceptorProviders,
